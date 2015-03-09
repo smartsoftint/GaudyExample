a4f42204e5084bda62d952909570505e010b49e6
smartsoftint <asegreda@smartsoftint.com>
2015-03-09 13:59:44 -0600

    fc

--- js/main.js
+++ js/main.js
@@ -18,10 +18,21 @@
     });
 	
 	
+	//MSG Swipped 
+	$( document ).on("swipeleft", ".Message" ,function(a,b,c){
+			console.log(JSON.stringify(b));
+		if(b["xAmount"] > 150){
+			alert(JSON.stringify(b));
+		}
+	});
+	
+	
 	
 })(jQuery);
 
 
 
 
-
+/*
+{	"startEvnt":{"position":{"x":538,"y":341},"offset":{"x":441,"y":228},"time":1425930563806,"target":{}}
+,"endEvnt":{"position":{"x":481,"y":336},"offset":{"x":384,"y":223},"time":1425930563917,"target":{}},"direction":"left","xAmount":57,"yAmount":5,"duration":111}*/
